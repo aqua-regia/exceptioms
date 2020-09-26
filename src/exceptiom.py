7906b11c1006c02d8d3ae7c54a0ea460d0e523dd
hassan_ashraf_ <hassan@coinswitch.co>
2020-09-26 22:01:05 +0530

    index out of bound error

--- src/exceptiom.py
+++ src/exceptiom.py
@@ -5,7 +5,7 @@ import random
 class Exceptioms(Exception):
     def __init__(self, *args, **kwargs):
         length_jokes = len(jokes)
-        joke_to_pick = random.randint(0, length_jokes)
+        joke_to_pick = random.randint(0, length_jokes-1)
         selected_jokes = jokes[joke_to_pick]
         Exception.__init__(self, selected_jokes, args, kwargs)
 
