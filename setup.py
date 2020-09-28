91aa33d473e1a9cf7fc8cff94e30b069a901eb10
hassan_ashraf_ <hassan@coinswitch.co>
2020-09-28 16:30:05 +0530

    Bump version: 1.0.0 → 1.1.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="exceptiom",
-    version="1.0.0",
+    version="1.1.0",
     description="Make python exceptions fun",
     long_description=README,
     long_description_content_type="text/markdown",
